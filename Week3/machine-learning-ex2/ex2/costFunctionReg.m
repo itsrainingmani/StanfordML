1e07b73923ac9e5d62f9509b1811ed9d94912011
Manikandan Sundararajan <mtiruman@buffalo.edu>
2017-11-02 22:26:29 -0400

    Finished Week 3 assignment

--- Week3/machine-learning-ex2/ex2/costFunctionReg.m
+++ Week3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,7 +18,10 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
-
+h = 1./(1 + e.^-(X*theta));
+theta(1) = 0;
+J = (1/m)*(-y'*log(h) - (1-y)'*log(1-h)) + (lambda/(2*m))*sum((theta.^2));
+grad = (1/m)*(X' * (h-y)) + (lambda/m)*theta;
 
 
 
